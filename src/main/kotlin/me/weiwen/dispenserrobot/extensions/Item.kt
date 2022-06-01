2ceed75a8d8a3ba0aa3554d7c65eff71fd954dfe
Goh Wei Wen <goweiwen@gmail.com>
2022-06-01 14:07:28 +0800

    Fix items not breaking in dispensers

--- src/main/kotlin/me/weiwen/dispenserrobot/extensions/Item.kt
+++ src/main/kotlin/me/weiwen/dispenserrobot/extensions/Item.kt
@@ -1,15 +1,16 @@
 package me.weiwen.dispenserrobot.extensions
 
+import org.bukkit.Material
 import org.bukkit.block.Container
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 import kotlin.random.Random
 
-fun ItemStack.damage(damage: Int) {
-    val itemMeta = itemMeta ?: return
+fun ItemStack.damage(damage: Int): Boolean {
+    val itemMeta = itemMeta ?: return true
     if (itemMeta.isUnbreakable) {
-        return
+        return true
     }
 
     val unbreaking = itemMeta.getEnchantLevel(Enchantment.DURABILITY)
@@ -17,9 +18,14 @@ fun ItemStack.damage(damage: Int) {
     if (Random.nextDouble(1.0) * (unbreaking + 1) < 1) {
         (itemMeta as? Damageable)?.let {
             it.damage += damage
+            if (it.damage >= type.maxDurability) {
+                return false
+            }
             this.itemMeta = itemMeta
         }
     }
+
+    return true
 }
 
 fun ItemStack.damage(damage: Int, container: Container): Boolean {
@@ -28,8 +34,11 @@ fun ItemStack.damage(damage: Int, container: Container): Boolean {
         return false
     }
 
-    this.damage(damage)
-    container.inventory.setItem(slot, this)
+    if (this.damage(damage)) {
+        container.inventory.setItem(slot, this)
+    } else {
+        container.inventory.setItem(slot, ItemStack(Material.AIR))
+    }
     return true
 }
 
